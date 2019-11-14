0424f72c4efe9b792d94b153ef85765f0f364eb2
Nss18031997 <shanmugasundaram.n@apsilabs.com>
2019-11-14 14:01:00 +0530

    Arraryprogram
    program to store a value  in array

---
+++ Array.java
@@ -0,0 +1,20 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		                                           
+	      int[] arr; 
+	       arr = new int[6]; 
+	      arr[0] = 10; 
+	      arr[1] = 20; 
+	      arr[2] = 30; 
+	      arr[3] = 40; 
+	      arr[4] = 50; 
+	      arr[5]=40;
+	      for (int i = 0; i < arr.length; i++) 
+	         System.out.println("The value in index " + i +  
+	                                      " : "+ arr[i]);  
+
+	}
+
+}
